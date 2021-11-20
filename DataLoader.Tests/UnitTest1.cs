1e33ed5d0c4ad8335b455407ebecfe712e950d09
Ignacy <ignacy130@gmail.com>
2021-11-20 15:45:32 +0100

    add test

--- DataLoader.Tests/UnitTest1.cs
+++ DataLoader.Tests/UnitTest1.cs
@@ -1,14 +1,42 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using Xunit;
 
 namespace DataLoader.Tests
 {
-    public class UnitTest1
+    public class DataLoaderTests
     {
         [Fact]
-        public void Test1()
+        public void ReadTitle_()
         {
 
         }
+    }
+
+    public class WebsiteReaderTests
+    {
+        [Theory]
+        [ClassData(typeof(TestFiles))]
+        public void ReadTitle_Test(string source)
+        {
+            var reader = new WebsiteReader();
+            reader.GetTitle(source);
+        }
+    }
+
+    public class TestFiles : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            var files = Directory.GetFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Data"));
+            foreach (var item in files)
+            {
+                yield return new string[] { File.ReadAllText(item) };
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
     }
 }
---
+++ DataLoader/WebsiteReader.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLoader
+{
+    public class WebsiteReader : IReadWebsite
+    {
+        public string GetTitle(string source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
