7d7da7ce13c9bb5a21cec54399af0deb75262b59
Ignacy <2983629+ignacy130@users.noreply.github.com>
2021-11-21 15:52:31 +0100

    add app tests to run real

---
+++ DataLoader.Tests/AppTests.cs
@@ -0,0 +1,29 @@
+﻿using DataLoader.Contract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DataLoader.Tests
+{
+    public class AppHttpClientFactory : IHttpClientFactory
+    {
+        public HttpClient Create()
+        {
+            return HttpClientFactory.Create();
+        }
+    }
+
+    public class AppTests
+    {
+        [Fact]
+        public async void RunApp()
+        {
+            var app = new App(new WebsiteLoader(new AppHttpClientFactory()), new WebsiteReader());
+            await app.SaveDataFor(new Uri("https://example.com/"));
+        }
+    }
+}
