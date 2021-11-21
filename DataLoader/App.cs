0f29d9c9e57a78636e7033f83ae8b224b8b8854b
Ignacy <2983629+ignacy130@users.noreply.github.com>
2021-11-21 12:11:22 +0100

    cleanup, add app class

---
+++ DataLoader/App.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DataLoader
+{
+    public class App
+    {
+        private readonly ILoadData _loader;
+        private readonly IReadWebsite _reader;
+        public App(ILoadData loader, IReadWebsite reader)
+        {
+            _loader = loader;
+            _reader = reader;
+        }
+
+        public async Task SaveDataFor(Uri url)
+        {
+            var source = await _loader.RequestData(url);
+            _reader.GetTitle(source);
+
+        }
+    }
+}
